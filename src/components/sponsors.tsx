73958c7272f6e2b0b8df6439821eebf4e3adcfb1
Xyncross1111 <anasmk2003@gmail.com>
2025-02-19 12:20:38 +0530

    update: sponsors

--- src/components/sponsors.tsx
+++ src/components/sponsors.tsx
@@ -6,21 +6,21 @@ export function Sponsors() {
     const sponsors = [
         "/assets/image/sponsors/burger_singh.png",
         "/assets/image/sponsors/IDP.png",
-        "/assets/image/sponsors/MICRO_SYSTEMS.png",
+        "/assets/image/sponsors/MICROSYSTEMS.png",
         "/assets/image/sponsors/PAREKH_BROTHERS.png",
         "/assets/image/sponsors/WCFL.png",
+        "/assets/image/sponsors/222_BACKYARD.png",
     ]
 
     // Duplicate the array for a continuous marquee effect
     const marqueeSponsors = [...sponsors, ...sponsors]
 
     return (
-        <div className="bg-black w-full py-12 overflow-hidden">
+        <div className="bg-black w-full py-6 overflow-hidden">
             <div className="max-w-7xl mx-auto px-4">
-                <h2 className="text-white text-4xl md:text-5xl font-bold text-center mb-16">
+                <h2 className="text-white text-4xl md:text-5xl font-bold text-center mb-6 ">
                     OUR SPONSORS
                 </h2>
-
                 <div className="relative">
                     {/* First Row */}
                     <div className="flex space-x-6 sm:space-x-12 animate-marquee">
