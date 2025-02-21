5c39b907cf97250c85767207093b11ec7b3338d8
Xyncross1111 <anasmk2003@gmail.com>
2025-02-22 02:41:53 +0530

    update: config

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,16 @@ const nextConfig = {
     images: {
         domains: ['i.imgur.com'],
     },
+    async headers() {
+        return [
+          {
+            source: "/assets/:path*",
+            headers: [
+              { key: "Cache-Control", value: "public, max-age=31536000, immutable" },
+            ],
+          },
+        ];
+      },
 };
 
 export default nextConfig;
